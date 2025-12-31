69501edf80d828bfb644e3747b8f5300d85a1624
Christophe Gakwaya <c.gakwaya@alustudent.com>
2025-12-31 11:06:33 +0200

    everything connected to deployed backend

--- server/src/config/cloudinary.ts
+++ server/src/config/cloudinary.ts
@@ -3,11 +3,11 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// --- ADD THESE DEBUG LOGS ---
-console.log("DEBUG CLOUDINARY CONFIG:");
-console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
-console.log("API Key:", process.env.CLOUDINARY_API_KEY);
-console.log("API Secret (First 3 chars):", process.env.CLOUDINARY_API_SECRET ? process.env.CLOUDINARY_API_SECRET.substring(0, 3) + "..." : "UNDEFINED");
+// // --- ADD THESE DEBUG LOGS ---
+// console.log("DEBUG CLOUDINARY CONFIG:");
+// console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
+// console.log("API Key:", process.env.CLOUDINARY_API_KEY);
+// console.log("API Secret (First 3 chars):", process.env.CLOUDINARY_API_SECRET ? process.env.CLOUDINARY_API_SECRET.substring(0, 3) + "..." : "UNDEFINED");
 // ----------------------------
 
 cloudinary.config({
